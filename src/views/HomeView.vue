8ca7dc4f6edd8ea8ff4b48800229f6eebfac9e13
KodaHye <kodhdh09@gmail.com>
2023-05-21 05:25:15 +0900

    refactor: fullpage 이슈 해결

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="fullpage">
-    <div class="section section1" style="background-image: url('/main_img/unicorn_1.jpeg'); background-size: 100% 100%;">
-      <!-- <h1>trip only for you</h1> -->
-      <div class = "init_screen">
+    <!-- 첫번째 Section -->
+    <div class="section" style="background-image: url('/main_img/unicorn_1.jpeg'); background-size: 100% 100%;">
+      <div class="section-content">
         <span style="font-size: 40pt;">
           Trip only for you
         </span>
       </div>
-
     </div>
-    <div class="section section2" style="background-image: url('/main_img/unicorn_2.jpeg'); background-size: 100% 100%;">
-      <div>
+
+    <!-- 두번째 Section -->
+    <div class="section" style="background-image: url('/main_img/unicorn_2.jpeg'); background-size: 100% 100%;">
+      <div class="section-content">
         <div>
           <span style="font-size: 28pt;">당신이 가고싶은 여행지를</span>
         </div>
@@ -18,47 +19,52 @@
         <div style="margin: 10px;"><span style="font-size: 17pt;">여행지 월드컵하러 가기</span></div>
       </div>
     </div>
-    <div class="section section3" style="background-image: url('/main_img/unicorn_3.jpeg'); background-size: 100% 100%;">
-      <div>
-        <span style="font-size: 17pt;">당신과 비슷한 사람들이 선택한 여행지입니다.</span>
+    <!-- 우리지역 관광지 끝 -->
+
+
+    <!-- 나만의 여행계획 시작-->
+    <div class="section" style="background-image: url('/main_img/unicorn_3.jpeg'); background-size: 100% 100%;">
+      <!-- 크기 설정하기, card 캐러셀같이 하거나 -->
+      <div class="container" style="margin-top: 120px;">
+        <div>
+          <span style="font-size: 17pt;">당신과 비슷한 사람들이 선택한 여행지입니다.</span>
+        </div>
+
+        <div class="row">
+          <div class="card" style="width: 18rem;">
+            <img src="" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+
+          <div class="card" style="width: 18rem;">
+            <img src="" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+
+          <div class="card" style="width: 18rem;">
+            <img src="" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
       </div>
-
-      <div class="row">
-        <div>
-          <b-card title="Card Title" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top
-            tag="article" style="max-width: 20rem;" class="mb-2">
-            <b-card-text>
-              Some quick example text to build on the card title and make up the bulk of the card's content.
-            </b-card-text>
-
-            <b-button href="#" variant="primary">Go somewhere</b-button>
-          </b-card>
-        </div>
-        <div>
-          <b-card title="Card Title" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top
-            tag="article" style="max-width: 20rem;" class="mb-2">
-            <b-card-text>
-              Some quick example text to build on the card title and make up the bulk of the card's content.
-            </b-card-text>
-
-            <b-button href="#" variant="primary">Go somewhere</b-button>
-          </b-card>
-        </div>
-        <div>
-          <b-card title="Card Title" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top
-            tag="article" style="max-width: 20rem;" class="mb-2">
-            <b-card-text>
-              Some quick example text to build on the card title and make up the bulk of the card's content.
-            </b-card-text>
-
-            <b-button href="#" variant="primary">Go somewhere</b-button>
-          </b-card>
-        </div>
-
-      </div>
-
-
-      <div class="footer" style="text-align: left;">
+      <!-- 나만의 여행계획 끝 -->
+      <footer class="p-4 mt-3 container-fluid"
+        style="position: absolute; bottom: 0; background-color: rgba(50, 50, 50, 0.1);">
         <nav>
           <a href="#">GitHub</a> |
           <a href="#">Blog</a>
@@ -68,54 +74,52 @@
           <span>주소 : 대전 유성구 동서대로 98-39</span><br>
           <span>Copyright 2023. ssafy. All Rights Reserved.</span><br>
         </p>
-      </div>
+      </footer>
     </div>
+
   </div>
 </template>
 
 <script>
-import fullpage from 'fullpage.js';
 
 export default {
   mounted() {
-    new fullpage('#fullpage', {
-      // fullPage.js 옵션 설정
-    });
+
   },
 };
 </script>
 
 <style scoped>
-#fullpage {
-  height: 100vh;
-}
-
 .section {
-  text-align: center;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
+  margin: 0;
+  padding: 0;
 }
 
-.footer {
-  position: absolute;
-  bottom: 0;
+.section-content {
+  position: relative;
+  text-align: center;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -100%);
+}
+
+.main-footer {
   left: 0;
   width: 100%;
   padding: 20px;
   background-color: #f5f5f5;
-  text-align: center;
-  z-index: 100000000;
 }
 
-.init_screen{
-	animation: fadein 2s;
+.init_screen {
+  animation: fadein 2s;
 }
 
 @keyframes fadein {
-	from { opacity: 0; }
-	to { opacity: 1; }
-}
-</style>
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+}</style>
