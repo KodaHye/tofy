c01b47b602fd1685a51ad1993b1326d2cba59a29
ki_mensoo <ekclstkfka5@gmail.com>
2023-05-22 16:02:16 +0900

    refactor: 카드 shadow 변경

--- src/views/AttractionWorldCupView.vue
+++ src/views/AttractionWorldCupView.vue
@@ -266,7 +266,7 @@ export default {
     height: 60vh; 
     width: 60vw; 
     background-size: cover;
-    box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+    box-shadow: rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px, rgba(0, 0, 0, 0.07) 0px 32px 64px;
     animation: move 1.0s alternate;
 }
 
