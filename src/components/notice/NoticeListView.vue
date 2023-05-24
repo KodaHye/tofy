9ad537e293c26aa613b65e89c6797707b874cc01
KodaHye <kodhdh09@gmail.com>
2023-05-24 23:36:34 +0900

    refactor: 공지사항 글 쓰기 버튼 user에 따라 유동적이게 변경

--- src/components/notice/NoticeListView.vue
+++ src/components/notice/NoticeListView.vue
@@ -8,59 +8,58 @@
                 <div><span>공지사항을 확인해주세요.</span></div>
 
             </div>
-            <div class="">
-                <div class="row align-self-center mb-2">
-                    <div class="col-md-2 text-start">
-                        <b-button v-if="this.userInfo.adminFl === Y" variant="outline-primary" @click="$router.push({ name: 'freeboardwrite' })">글쓰기</b-button>
-                    </div>
-                    <div class="col-md-3"></div>
-                    <div class="col-md-7">
-                        <div class="row" style="float: right;">
-                            <div style="padding-right: 5px;">
-                                <b-form-select name="sort_list" id="sort_list" style="width: 130px;" aria-label="정렬조건"
-                                    :value="null">
-                                    <option value="null">게시글 정렬</option>
-                                    <option value="notice">최신 순</option>
-                                    <option value="sort_hit">조회수 순</option>
-                                </b-form-select>
-                            </div>
-                            <div style="padding-right: 5px;">
-                                <!-- <input type="hidden" name="action" value="notice" /> <input
+            <div class="row align-self-center mb-2">
+                <div class="col-md-2 text-start">
+                    <b-button v-if="this.userInfo.adminFl == 'Y'" variant="outline-primary"
+                        @click="$router.push({ name: 'freeboardwrite' })">글쓰기</b-button>
+                </div>
+                <div class="col-md-3"></div>
+                <div class="col-md-7">
+                    <div class="row" style="float: right;">
+                        <div style="padding-right: 5px;">
+                            <b-form-select name="sort_list" id="sort_list" style="width: 130px;" aria-label="정렬조건"
+                                :value="null">
+                                <option value="null">게시글 정렬</option>
+                                <option value="notice">최신 순</option>
+                                <option value="sort_hit">조회수 순</option>
+                            </b-form-select>
+                        </div>
+                        <div style="padding-right: 5px;">
+                            <!-- <input type="hidden" name="action" value="notice" /> <input
 						type="hidden" name="pgno" value="1" />  -->
-                                <b-form-select name="key" id="key" style="width: 130px" aria-label="검색조건" :value="null">
-                                    <option value="null">검색조건</option>
-                                    <option value="article_no">글번호</option>
-                                    <option value="subject">제목</option>
-                                    <option value="user_id">작성자</option>
-                                </b-form-select>
-                            </div>
-                            <div style="padding-right: 5px;">
-                                <b-input type="text" name="word" id="word" style="width: 150px" placeholder="검색어..." />
-                                <!-- <b-button id="btn-search" class="btn btn-dark" type="button">검색</b-button> -->
-                            </div>
-                            <div style="padding-right: 5px;">
-                                <b-button>검색</b-button>
-                            </div>
+                            <b-form-select name="key" id="key" style="width: 130px" aria-label="검색조건" :value="null">
+                                <option value="null">검색조건</option>
+                                <option value="article_no">글번호</option>
+                                <option value="subject">제목</option>
+                                <option value="user_id">작성자</option>
+                            </b-form-select>
+                        </div>
+                        <div style="padding-right: 5px;">
+                            <b-input type="text" name="word" id="word" style="width: 150px" placeholder="검색어..." />
+                            <!-- <b-button id="btn-search" class="btn btn-dark" type="button">검색</b-button> -->
+                        </div>
+                        <div style="padding-right: 5px;">
+                            <b-button>검색</b-button>
                         </div>
                     </div>
                 </div>
-                <div class="row align-self-center mb-2"></div>
-                <table class="table table-hover">
-                    <thead>
-                        <tr class="text-center">
-                            <th scope="col">글번호</th>
-                            <th scope="col">제목</th>
-                            <th scope="col">작성자</th>
-                            <th scope="col">조회수</th>
-                            <th scope="col">작성일</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <notice-list-item v-for="(board, i) in noticeList" :key="i" :board="board"></notice-list-item>
-                        <!-- <free-board-list-item v-for="(board, i) in noticeList" :key="i" :board="board"></free-board-list-item> -->
-                    </tbody>
-                </table>
             </div>
+            <div class="row align-self-center mb-2"></div>
+            <table class="table table-hover">
+                <thead>
+                    <tr class="text-center">
+                        <th scope="col">글번호</th>
+                        <th scope="col">제목</th>
+                        <th scope="col">작성자</th>
+                        <th scope="col">조회수</th>
+                        <th scope="col">작성일</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <notice-list-item v-for="(board, i) in noticeList" :key="i" :board="board"></notice-list-item>
+                    <!-- <free-board-list-item v-for="(board, i) in noticeList" :key="i" :board="board"></free-board-list-item> -->
+                </tbody>
+            </table>
         </div>
     </div>
 </template>
@@ -92,7 +91,7 @@ export default {
         //         console.log(err)
         //     }
         // )
-        http.get('/notice').then(({data}) => {
+        http.get('/notice').then(({ data }) => {
             this.noticeList = data;
         })
     },
@@ -103,5 +102,4 @@ export default {
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
