b2de7252ff52c6e612a35cd1e599c8af484b19f8
KodaHye <kodhdh09@gmail.com>
2023-05-24 09:09:12 +0900

    feat: 댓글 목록 조회

--- src/components/freeboard/CommentItem.vue
+++ src/components/freeboard/CommentItem.vue
@@ -1,17 +1,19 @@
 <template>
     <div>
         <div class="row" style="display: flex; justify-content: space-between; padding: 0 20px;">
-            <div>
+            <div style="padding-bottom: 5px;">
+                <img class="avatar me-2 float-md-start bg-light p-2"
+                                src="https://raw.githubusercontent.com/twbs/icons/main/icons/person-fill.svg" />
                 <!-- 작성자 이름 -->
-                <span>작성자: </span>{{ comment.userNo }}
+                {{ comment.user.userNm }}
             </div>
             <div class="row" style="padding-right: 20px">
                 <div style>{{ comment.commentCreate }}</div>
-                <div style="margin-left: 5px;">수정</div>
-                <div style="margin-left: 5px;">삭제</div>
+                <div style="margin-left: 5px;"><a href="#" style="color: black;">수정</a></div>
+                <div style="margin-left: 5px;"><a href="#" style="color: black;">삭제</a></div>
             </div>
         </div>
-        <div style="padding: 0px 5px">{{ comment.commentContent }}</div>
+        <div style="padding: 0px 15px">{{ comment.commentContent }}</div>
         <hr>
     </div>
 </template>
@@ -29,9 +31,12 @@ export default {
             message: '',
         };
     },
-    created() { },
+    created() {
+        console.log(this.comment)
+     },
     methods: {},
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>
