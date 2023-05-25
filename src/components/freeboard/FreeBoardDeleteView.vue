481dccd001d74e0c26c92a07e1e8cd380a0d356e
KodaHye <kodhdh09@gmail.com>
2023-05-25 22:31:20 +0900

    feat: 글 삭제 confirm 추가

--- src/components/freeboard/FreeBoardDeleteView.vue
+++ src/components/freeboard/FreeBoardDeleteView.vue
@@ -13,11 +13,14 @@ export default {
         };
     },
     created() {
-        console.log(this.$route.params.boardno);
-        http.delete(`/freeboard/${this.$route.params.boardno}`).then(({ data }) => {
-            this.board = data
-        })
-        this.$router.push({ name: "freeboardList" });
+        if (confirm('정말 글을 삭제하시겠습니까?')) {
+            console.log(this.$route.params.boardno);
+            http.delete(`/freeboard/${this.$route.params.boardno}`).then(({ data }) => {
+                this.board = data
+            })
+            alert("글이 삭제되었습니다.")
+            this.$router.push({ name: "freeboardList" });
+        } else this.$router.back()
     },
     methods: {},
 };
